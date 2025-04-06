fb79adf8bcd73688f93ab6019d97d0de52004dc4
RaVa03 <raluca.valeriarv@gmail.com>
2025-04-06 12:18:47 +0300

    Animations connected with script

--- Assets/Scripts/EnemyHealth.cs
+++ Assets/Scripts/EnemyHealth.cs
@@ -3,18 +3,45 @@ using UnityEngine;
 public class EnemyHealth : MonoBehaviour
 {
     public float health;
+    [SerializeField]
+    private Animator animator;
+    private bool die = false;
+    // void Start()
+    // {
+    //     health = 100;
+    // }
 
-    void Start()
+    // public void LoseLife(float damage)
+    // {
+    //     health -= damage;
+    //     if (health < 0)
+    //     {
+    //         Destroy(gameObject);
+    //     }
+    // }
+    void Awake()
     {
-        health = 100;
+        animator = GetComponent<Animator>();
     }
 
     public void LoseLife(float damage)
     {
+        if (die) return;
+
         health -= damage;
-        if (health < 0)
+
+        if (health <= 0)
         {
-            Destroy(gameObject);
+            die = true;
+            animator.SetTrigger("die"); 
+            GetComponent<UnityEngine.AI.NavMeshAgent>().enabled = false;
+            StartCoroutine(DestroyAfterDeath()); 
         }
+    }
+
+    private System.Collections.IEnumerator DestroyAfterDeath()
+    {
+        yield return new WaitForSeconds(3f);
+        Destroy(gameObject);
     }
 }
